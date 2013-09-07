e77e7da5fcbf713967fa355d99f1ff5f06ac8f0d
John Lapeyre <jlapeyre@cpan.org>
2013-09-07 15:38:37 +0200

    more doc edits

--- prime-tables.lisp
+++ prime-tables.lisp
@@ -6,25 +6,32 @@
 ;;; (at your option) any later version.
 (in-package :prime-tables)
 
-;;; Find values of prime pi by combining computation
-;;;  with table lookup.
+;;; load prime pi tables and provide lookup.
 ;;;
-;;; This code loads the tables and provides a lisp interface to
-;;; the tables.
+;;; This code loads the tables and provides a lisp interface to the
+;;; tables.
 ;;;
-;;; The main interface function is look-up-pi-and-rem (x),
-;;; which finds prime_pi(x1) for the largest x1
-;;; in the table, such that x1 <= x. It returns a list
-;;; of prime_pi(x1), x1, and x-x1.
-;;; This information can be used by the caller to know that
-;;; primes must be searched for between x1 and x.
+;;; The main interface function is look-up-pi-and-rem (x), which finds
+;;; prime_pi(x1) for the largest x1 in the table, such that x1<=x. It
+;;; returns a list of prime_pi(x1), x1, and x-x1.  This information
+;;; can be used by the caller to know that primes must be searched for
+;;; between x1 and x.
+;;;
+;;; It would be useful to implement caching. The idea is that a
+;;; combination of lookup and sieving should be cached for later
+;;; use. I guess that a self-balancing binary tree would be the
+;;; appropriate data structure. The search would need to return the
+;;; largest node smaller than or equal to the node being searched
+;;; for. I tried to work with some code for AVL trees from the
+;;; internet, but I stopped because of asdf hell combined with maxima
+;;; hell.
 
 (defstruct (prime-table)
   (data '#() :type array)
-  (exp 0 :type real)
-  (base 10 :type fixnum)
-  (incr 0 :type real)
-  (max 0 :type real))
+  (exp 0     :type real)
+  (base 10   :type fixnum)
+  (incr 0    :type real)
+  (max 0     :type real))
 
 ;; this is referenced in data table files
 (defparameter *prime-tables-structs* '())
@@ -35,7 +42,6 @@
 (defun set-prime-table (data-array  exp)
   (let* ((base 10)
          (incr (expt base exp)))
-;;         (data-array (prime-data-list-to-array data-list)))
     (make-prime-table
      :data data-array
      :exp exp
@@ -44,12 +50,11 @@
      :max (* incr (1- (length data-array))))))
 
 (defun look-up-pi-and-rem-in-table (x table)
- "Return a list (pi-tab min rem), where `pi-tab' is
+  "Return a list (pi-tab min rem), where `pi-tab' is
   the value of prime pi function at argument `min',
   and `rem'= `x'-`min'"
   (let ((incr (prime-table-incr table)))
     (multiple-value-bind (indv remv) (floor x incr)
-;;      (format t "incr ~a indv ~a  remv ~a~%" incr indv remv)
       (list (aref (prime-table-data table) indv)
             (1+ (* incr indv))
             remv))))
@@ -57,13 +62,12 @@
 (defun look-up-pi-and-rem (x)
   (let ((pos (position-if #'(lambda (y) (< x y)) *prime-table-maxes*)))
     (unless pos (format t "*** x is too large~%"))
-;;    (format t "** using table with incr ~a~%" (prime-table-incr (elt *prime-tables-structs* pos)))
     (look-up-pi-and-rem-in-table x (elt *prime-tables-structs* pos))))
 
 (defun setup-prime-pi-tables-2 ()
   (let ((res) (tables (reverse *prime-pi-tables*))) ; they were pushed onto the list
-    (loop for i from 1 to (length tables) do
-      (push (set-prime-table (pop tables) i) res))
+    (loop :for i :from 1 :to (length tables) :do
+          (push (set-prime-table (pop tables) i) res))
     (nreverse res)))
 
 (defun setup-prime-pi-tables-1 ()
@@ -79,6 +83,8 @@
 
 (setup-prime-pi-tables)
 
+;;; Only commented out code below this line
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; looks like I don't need this now
 ;; The tables are constructed as arrays.
 ;(defun prime-data-list-to-array (data-list)
